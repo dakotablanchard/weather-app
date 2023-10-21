607db2c606d4d26207dd84d988100c4ed8947325
Dakota Blanchard <dakotablanchard2699@gmail.com>
2023-10-20 20:38:44 -0400

    got current weather and location to log

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,55 @@ var date = document.querySelector("#date")
 var apiKey = "b3b885146dce3d4c2a9d8e921432d8fb"
 var today = dayjs()
 
+var locationEl = document.getElementById("location")
+var currentTempEl = document.getElementById("current-temp")
+var currentWindEl = document.getElementById("current-wind")
+var currentHumidityEl = document.getElementById("current-humidity")
+
+var currentWeatherArray = []
+
 // Set current date on screen using dayjs
 date.textContent = today.format("MMMM D, YYYY")
 
-function searchCity() {
+
+function searchCity() { 
     var zipCode = document.querySelector("#zip-search").value
     var getCoordinates = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + ",US&appid=" + apiKey
 
-    fetch(getCoordinates,)
+    fetch(getCoordinates)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            // console.log(data.lat, data.lon)
+            var lat = data.lat
+            var lon = data.lon
+            getCurrentWeather(lat, lon)
+        });
+        
+        
+    }
+    
+function getCurrentWeather(lat, lon) {
+        var currentWeatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial"
+
+        fetch(currentWeatherEndpoint)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var location = data.name
+            var currentTemp = data.main.temp + " °F"
+            var currentWind = data.wind.speed + " MPH"
+            var currentHumidity = data.main.humidity + "%"
+            console.log(data.name)
+            logCurrentWeather(location, currentTemp, currentWind, currentHumidity)
         });
 
-    var getCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
+}
 
+function logCurrentWeather(location, temp, wind, humidity) {
+    locationEl.textContent = location
+    currentTempEl.textContent = temp
+    currentWindEl.textContent = wind
+    currentHumidityEl.textContent = humidity
 }
